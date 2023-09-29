3f96d41ae1fe5c7ef00882bcc5f4a38e716116c8
juiceman1014 <justintran1014@gmail.com>
2023-09-28 23:06:30 -0700

    marker test function implemented

--- javascript.js
+++ javascript.js
@@ -42,3 +42,27 @@ const gameFlowModule = (() => {
         endGame,
     }
 })();
+
+//create an event listener that allows user to place marker
+//create a reference to each grid item
+const squares = document.querySelectorAll(".grid-item");
+
+//player 1 
+//player 2 
+
+//console.log(gameFlowModule.currentPlayer);
+
+
+let player1 = playerFactory("Player1", "X");
+
+gameFlowModule.currentPlayer = player1;
+
+squares.forEach((square) =>{
+    square.addEventListener("click", () => {
+        square.textContent = "x";
+    })
+})
+
+//if currentPlayer === player 1; next click places an X
+//if currentPlayer === player 2; next click places an O
+
