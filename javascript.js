4bf876ac181a73bcaff7f302472e56d3694758c3
juiceman1014 <justintran1014@gmail.com>
2023-10-01 17:58:15 -0700

    players can now place x and o's onto the board.

--- javascript.js
+++ javascript.js
@@ -36,6 +36,10 @@ const gameFlowModule = (() => {
         isGameActive = false;
     };
 
+    const checkWin = () => {
+
+    }
+
     const getIsGameActive = () => isGameActive;
 
     const getCurrentPlayerName = () => currentPlayer.name;
@@ -58,11 +62,37 @@ gameFlowModule.startGame();
 console.log(gameFlowModule.getCurrentPlayerName());
 console.log(gameFlowModule.getCurrentPlayerSymbol());
 
+//gameFlowModule.startGame();
+    // square.addEventListener("click", () => {
+    //     square.textContent = "x";
+    // })
+    //start game
+    //while isGameActive = true;
+        //if currentPlayer === player1
+            //let player1 play
+            //switch to secondPlayer
+        //else if currentPlayer === player2
+            //let player2 play
+            //switch to first player
+
 squares.forEach((square) =>{
-    //gameFlowModule.startGame();
-    square.addEventListener("click", () => {
-        square.textContent = "x";
-    })
+        
+    gameFlowModule.startGame();
+    if(gameFlowModule.getIsGameActive() === true){
+        if (gameFlowModule.getCurrentPlayerName() === "Player1"){
+            square.addEventListener("click", () =>{
+                square.textContent = gameFlowModule.getCurrentPlayerSymbol();
+                gameFlowModule.switchPlayer();
+            })
+        }else if(gameFlowModule.getCurrentPlayerName() === "Player2"){
+            square.addEventListener("click", () =>{
+                square.textContent = gameFlowModule.getCurrentPlayerSymbol();
+                gameFlowModule.switchPlayer();
+            })
+        }
+    }
+
+
 })
 
 //if currentPlayer === player 1; next click places an X
